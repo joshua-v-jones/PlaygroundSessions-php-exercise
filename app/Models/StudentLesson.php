5b2e1c2cc7f751ce1a9871c326a5c158c97c8ea0
joshua-v-jones <joshua.v.jones91@gmail.com>
2021-02-11 20:15:17 +0100

    Adding in the student lesson class that will be used to extract the data and transform it into the correct data structure.

---
+++ app/Models/StudentLesson.php
@@ -0,0 +1,78 @@
+<?php
+namespace app\Models;
+
+/**
+ *
+ * * Small model class that will harness the existing eloquent 
+ *  models to create the desired data structure for a single student lesson.
+ *
+ * @author jj
+ *        
+ */
+class StudentLesson
+{
+
+    //Integer used to store the lesson ID of this particular student lesson
+    private $lesson_id;
+    //String used to get the difficulty of the lesson 
+    //and transform it to the desired data structure
+    private $difficulty;
+    //Boolean used to get the completioness of a lesson into the correct structure
+    private $isComplete;
+    
+    /**
+     */
+    public function __construct()
+    {
+        
+    }
+    
+    /**
+     * @return mixed
+     */
+    public function getLesson_id()
+    {
+        return $this->lesson_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDifficulty()
+    {
+        return $this->difficulty;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIsComplete()
+    {
+        return $this->isComplete;
+    }
+
+    /**
+     * @param mixed $lesson_id
+     */
+    public function setLesson_id($lesson_id)
+    {
+        $this->lesson_id = $lesson_id;
+    }
+
+    /**
+     * @param mixed $difficulty
+     */
+    public function setDifficulty($difficulty)
+    {
+        $this->difficulty = $difficulty;
+    }
+
+    /**
+     * @param mixed $isComplete
+     */
+    public function setIsComplete($isComplete)
+    {
+        $this->isComplete = $isComplete;
+    }
+}
+
