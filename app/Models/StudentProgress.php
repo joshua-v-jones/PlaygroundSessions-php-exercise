27458bd1ee88cb5ce1689b14bf9fba7f16820baf
joshua-v-jones <joshua.v.jones91@gmail.com>
2021-02-11 20:14:05 +0100

    Small change to the progress comment.

--- app/Models/StudentProgress.php
+++ app/Models/StudentProgress.php
@@ -5,12 +5,18 @@ namespace App\Models;
 
 /**
  * Small model class that will harness the existing eloquent 
- * models to create the desired data structure
+ * models to create the desired data structure for all lessons by a student.
+ * E.G. The students progress.
  * 
  * @author Josh
  *
  */
 class StudentProgress
 {
+    function getLessons()
+    {
+        
+    }
+ 
     
 }
