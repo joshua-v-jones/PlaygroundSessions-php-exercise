60590c390205ebe920c7416d826c12b1a39f4d2e
joshua-v-jones <joshua.v.jones91@gmail.com>
2021-02-11 19:55:42 +0100

    Adding in the empty studentprogress model that will be used to create the desired data structure using the existing eloquent models.

---
+++ app/Models/StudentProgress.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Models;
+
+/**
+ * Small model class that will harness the existing eloquent 
+ * models to create the desired data structure
+ * 
+ * @author Josh
+ *
+ */
+class StudentProgress
+{
+    
+}
